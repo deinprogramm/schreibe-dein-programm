d49cbc566da71d517ce673d494746d83b93c5c1d
Mike Sperber <sperber@deinprogramm.de>
2019-04-14 13:59:19 +0200

    WIP Zusammengesetzte Daten

---
+++ i1zus/computer.rkt
@@ -0,0 +1,62 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname computer) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Ein Computer besteht aus:
+; - Prozessor
+; - Hauptspeicher-Kapazität in Gbyte
+; - Festplatten-Kapazität in Gbyte
+(define-record-functions computer
+  make-computer
+  (computer-processor  string)
+  (computer-ram        natural)
+  (computer-hard-drive natural))
+
+(: make-computer (string natural natural -> computer))
+(: computer-processor (computer -> string))
+(: computer-ram (computer -> natural))
+(: computer-hard-drive (computer -> natural))
+
+; Cell, 4 Gbyte RAM, 1000 Gbyte Festplatte
+(define gamer (make-computer "Cell" 4 1000))
+
+; Xeon, 2 Gbyte RAM, 500 Gbyte Festplatte
+(define workstation (make-computer "Xeon" 2 500))
+
+; Gesamtspeicher berechnen
+(: total-memory (computer -> natural))
+
+(check-expect (total-memory workstation) 502)
+(check-expect (total-memory gamer) 1004)
+
+(define total-memory
+  (lambda (c)
+    (+ (computer-ram c)
+       (computer-hard-drive c))))
+
+; Ein Modell ist eins der folgenden:
+; - Billigmodell
+; - Profi-Modell
+; - Gamer-Modell
+(define model
+  (signature
+   (one-of "cheap" "professional" "gamer")))
+
+; Standard-Computer zusammenstellen
+(: standard-computer (model -> computer))
+
+(check-expect (standard-computer "cheap")
+              (make-computer "Sempron" 2 500))
+(check-expect (standard-computer "professional")
+              (make-computer "Xeon" 4 1000))
+(check-expect (standard-computer "gamer")
+              (make-computer "Quad" 4 750))
+
+(define standard-computer
+  (lambda (k)
+    (cond
+      ((string=? k "cheap")
+       (make-computer "Sempron" 2 500))
+      ((string=? k "professional")
+       (make-computer "Xeon" 4 1000))
+      ((string=? k "gamer")
+       (make-computer "Quad" 4 750)))))
