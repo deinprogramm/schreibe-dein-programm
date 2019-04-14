06c74692194650d3499a1f89d9a07c0f504d3ad2
Mike Sperber <sperber@deinprogramm.de>
2019-04-14 14:07:47 +0200

    WIP zusammengesetzte Daten

---
+++ i1zus/wallclock-time.rkt
@@ -0,0 +1,43 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname wallclock-time) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Eine Uhrzeit besteht aus Stunde und Minute.
+(define-record-functions wallclock-time
+  make-wallclock-time
+  (wallclock-time-hour   natural)
+  (wallclock-time-minute natural))
+
+(: make-wallclock-time (natural natural -> wallclock-time))
+(: wallclock-time-hour (wallclock-time -> natural))
+(: wallclock-time-minute (wallclock-time -> natural))
+
+(define wt1 (make-wallclock-time 11 55)) ; fünf vor zwölf
+(define wt2 (make-wallclock-time 0 0)) ; Mitternacht
+(define wt3 (make-wallclock-time 1 1)) ; 1 Uhr 1
+
+; Minuten seit Mitternacht berechnen
+(: minutes-since-midnight (wallclock-time -> natural))
+
+(check-expect (minutes-since-midnight wt1) (+ (* 11 60) 55))
+(check-expect (minutes-since-midnight wt2) 0)
+(check-expect (minutes-since-midnight wt3) 61)
+
+(define minutes-since-midnight
+  (lambda (wt)
+    (+ (* 60 (wallclock-time-hour wt))
+       (wallclock-time-minute wt))))
+
+; Aus Minuten seit Mitternacht die Uhrzeit berechnen
+(: minutes-since-midnight->wallclock-time (natural -> wallclock-time))
+
+(check-expect (minutes-since-midnight->wallclock-time (+ (* 11 60) 55))
+              wt1)
+(check-expect (minutes-since-midnight->wallclock-time 0)
+              wt2)
+(check-expect (minutes-since-midnight->wallclock-time 61)
+              wt3)
+
+(define minutes-since-midnight->wallclock-time
+  (lambda (msm)
+    (make-wallclock-time (quotient msm 60) (remainder msm 60))))
+
