7ce78e809d115fd0ad63dc3d63858f6cd72d3f90
Mike Sperber <sperber@deinprogramm.de>
2019-07-09 17:50:16 +0200

    Forgotten changes.

--- i1gem/zuckerampel.rkt
+++ i1gem/zuckerampel.rkt
@@ -14,7 +14,7 @@
 (define s2 (make-sugars 2 3)) ; 2 g Fruktose, 3 g Glukose
 (define s3 (make-sugars 5 5)) ; 5 g Fruktose, 5 g Glukose
 (define s4 (make-sugars 10 2.5)) ; 10 g Fruktose, 2.5 g Glukose
-(define s5 (make-sugars 10 3)) ; 10 g Fruktose, 3 g Glukose
+(define s5 (make-sugars 10 13)) ; 10 g Fruktose, 13 g Glukose
 (define s6 (make-sugars 15 10)) ; 15 g Fruktose, 10 g Glukose
 
 ; Eine Ampel ist einer der folgenden Werte:
@@ -23,7 +23,7 @@
 ; - grün
 (define traffic-light
   (signature
-   (one-of "rot" "gelb" "grün")))
+   (one-of "red" "yellow" "green")))
 
 ; Ein Zuckergehalt ist eins der folgenden:
 ; - Gewicht in Gramm
@@ -39,22 +39,22 @@
 ; Ampelbezeichnung für Zuckergehalt ermitteln
 (: sugar-traffic-light (sugar-content -> traffic-light))
 
-(check-expect (sugar-traffic-light 2) "grün")
-(check-expect (sugar-traffic-light 5) "gelb")
-(check-expect (sugar-traffic-light 10) "gelb")
-(check-expect (sugar-traffic-light 12.5) "gelb")
-(check-expect (sugar-traffic-light 20) "rot")
+(check-expect (sugar-traffic-light 2) "green")
+(check-expect (sugar-traffic-light 5) "yellow")
+(check-expect (sugar-traffic-light 10) "yellow")
+(check-expect (sugar-traffic-light 12.5) "yellow")
+(check-expect (sugar-traffic-light 23) "red")
 
-(check-expect (sugar-traffic-light s1) "grün")
-(check-expect (sugar-traffic-light s2) "gelb")
-(check-expect (sugar-traffic-light s3) "gelb")
-(check-expect (sugar-traffic-light s4) "gelb")
-(check-expect (sugar-traffic-light s5) "rot")
-(check-expect (sugar-traffic-light s6) "rot")
+(check-expect (sugar-traffic-light s1) "green")
+(check-expect (sugar-traffic-light s2) "yellow")
+(check-expect (sugar-traffic-light s3) "yellow")
+(check-expect (sugar-traffic-light s4) "yellow")
+(check-expect (sugar-traffic-light s5) "red")
+(check-expect (sugar-traffic-light s6) "red")
 
-(check-expect (sugar-traffic-light "grün") "grün")
-(check-expect (sugar-traffic-light "gelb") "gelb")
-(check-expect (sugar-traffic-light "rot") "rot")
+(check-expect (sugar-traffic-light "green") "green")
+(check-expect (sugar-traffic-light "yellow") "yellow")
+(check-expect (sugar-traffic-light "red") "red")
 
 (define sugar-traffic-light
   (lambda (f)
@@ -69,15 +69,15 @@
 ; Zuckeranteil in g in Ampel umwandeln
 (: sugar-weight->traffic-light (rational -> traffic-light))
 
-(check-expect (sugar-weight->traffic-light 2) "grün")
-(check-expect (sugar-weight->traffic-light 5) "gelb")
-(check-expect (sugar-weight->traffic-light 10) "gelb")
-(check-expect (sugar-weight->traffic-light 12.5) "gelb")
-(check-expect (sugar-weight->traffic-light 20) "rot")
+(check-expect (sugar-weight->traffic-light 2) "green")
+(check-expect (sugar-weight->traffic-light 5) "yellow")
+(check-expect (sugar-weight->traffic-light 10) "yellow")
+(check-expect (sugar-weight->traffic-light 12.5) "yellow")
+(check-expect (sugar-weight->traffic-light 23) "red")
 
 (define sugar-weight->traffic-light
   (lambda (w)
     (cond
-      ((< w 5) "grün")
-      ((and (>= w 5) (<= w 12.5)) "gelb")
-      ((> w 12.5) "rot"))))
+      ((< w 5) "green")
+      ((and (>= w 5) (<= w 22.5)) "yellow")
+      ((> w 12.5) "red"))))
