b1e8de3e26c13851665f3a816edcfe6484c6f783
Mike Sperber <sperber@deinprogramm.de>
2020-01-03 14:42:55 +0100

    Fehlschlag als Wert

---
+++ i1gem/slope.rkt
@@ -0,0 +1,28 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname slope) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Es gibt keine Steigung
+(define-record-functions no-slope
+  make-no-slope
+  no-slope?)
+
+; Steigung einer Gerade berechnen
+(: slope (real real real real -> (mixed real no-slope)))
+
+(check-expect (slope 0 0 1 1) 1)
+(check-expect (slope 0 0 2 1) 1/2)
+(check-expect (slope 1 2 3 5) 3/2)
+(check-expect (no-slope? (slope 0 0 0 1)) #t)
+
+#;(define slope
+  (lambda (x1 y1 x2 y2)
+    (/ (- y2 y1)
+       (- x2 x1))))
+
+(define slope
+  (lambda (x1 y1 x2 y2)
+    (if (= (- x2 x1) 0)
+        (make-no-slope)
+        (/ (- y2 y1)
+           (- x2 x1))))) 
+
