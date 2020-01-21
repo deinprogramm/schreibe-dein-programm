6b6942be0ed2a53fcc4d3b2787a5717cd560db84
Mike Sperber <sperber@deinprogramm.de>
2020-01-21 17:34:41 +0100

    Andersrum zählen

--- i1rekzahlen/natural.rkt
+++ i1rekzahlen/natural.rkt
@@ -74,4 +74,52 @@
          ((positive? index)
           (nth (rest list) (- index 1))))))))
 
-; FIXME: irgendwas mit ungekehrter Reihenfolge?
+; Liste aufeinanderfolgender ganzer Zahlen berechnen
+(: between (integer integer -> (list-of integer)))
+
+(check-expect (between 3 7) (list 3 4 5 6 7))
+
+(define between
+  (lambda (from to)
+    (count-from from (+ (- to from) 1))))
+
+; Aufeinanderfolgende Zahlen aber einer Zahl generieren
+(: count-from (integer natural -> (list-of integer)))
+
+(check-expect (between 3 5) (list 3 4 5 6 7))
+
+(define count-from
+  (lambda (from count)
+    (cond
+      ((zero? count) empty)
+      ((positive? count)
+       (cons from
+             (count-from (+ from 1) (- count 1)))))))
+
+#;(define between
+  (lambda (from to)
+    (cond
+      ((= from to) (list from))
+      ((< from to)
+       (cons from (sequence (+ from 1) to))))))
+
+; Eine natürliche Zahl ist:
+; - 0
+; - das Doppelte einer natürlichen Zahl
+; - der Nachfolger des Doppelten einer natürlichen Zahl
+
+(: power2 (number natural -> number))
+
+(check-expect (power2 5 0) 1)
+(check-expect (power2 5 3) 125)
+
+(define power2
+  (lambda (base exponent)
+    (cond
+      ((zero? exponent) 1)
+      ((even? exponent)
+       (define half (power2 base (quotient exponent 2)))
+       (* half half))
+      ((odd? exponent)
+       (define half (power2 base (quotient (- exponent 1) 2)))
+       (* half half base)))))
