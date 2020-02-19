91ed3df22e8819180aed8ab56020595fbb72304b
Mike Sperber <sperber@deinprogramm.de>
2020-02-19 16:27:59 +0100

    fold fertig

--- i1hop/higher-order.rkt
+++ i1hop/higher-order.rkt
@@ -37,3 +37,29 @@
       ((cons? list)
        (for-cons (first list)
                  (xxx for-empty for-cons (rest list)))))))
+
+; im Stepper:
+; (xxx 0 + (list 1 2 3))
+
+(: list-fold (%a (%b %a -> %a) (list-of %b) -> %a))
+
+(define list-fold
+  (lambda (for-empty for-cons list)
+    (cond
+      ((empty? list) for-empty)
+      ((cons? list)
+       (for-cons (first list)
+                 (list-fold for-empty for-cons (rest list)))))))
+
+(: list-apply ((%a -> %b) (list-of %a) -> (list-of %b)))
+
+(check-expect (list-apply even? (list 1 2 3))
+              (list #f #t #f))
+
+(define list-apply
+  (lambda (f list)
+    (list-fold empty
+               (lambda (first-list result)
+                 (cons (f first-list)
+                       result))
+               list)))
