b271a55115e24a73aac53c952f97bf28e7acb92a
Mike Sperber <sperber@deinprogramm.de>
2021-08-25 17:37:32 +0200

    #lang

--- i1tree/binary-tree.rkt
+++ i1tree/binary-tree.rkt
@@ -1,6 +1,4 @@
-;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
-;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
-#reader(lib "vanilla-reader.rkt" "deinprogramm" "sdp")((modname binary-tree) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+#lang deinprogramm/sdp/vanilla
 ; Binärbäume
 
 ; Ein Knoten besteht aus
